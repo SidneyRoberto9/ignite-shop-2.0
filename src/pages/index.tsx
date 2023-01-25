3f7ee7d0f2740d65016bc645f24ad8208082174b
SidneyRoberto9 <sidneyrpsilva@gmail.com>
2023-01-25 16:36:40 -0300

    💄 style: Style Home Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,89 @@
+import { useKeenSlider } from "keen-slider/react";
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { Bag } from "phosphor-react";
+import React, { ButtonHTMLAttributes, MouseEvent } from "react";
+import Stripe from "stripe";
 
-export default function Home() {
+import { stripe } from "../lib/stripe";
+import { HomeContainer, Icon, Product } from "../styles/pages/home";
+import { priceFormatter } from "../util/format";
+
+interface HomeProps {
+  products: {
+    id: string
+    name: string
+    price: string
+    imageUrl: string
+  }[]
+}
+
+export default function Home({ products }: HomeProps) {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  })
+
+  function handleAddToCart(event: React.MouseEvent<HTMLButtonElement>) {
+    event.preventDefault()
+    console.log('add to cart')
+  }
+
   return (
     <>
       <Head>
         <title>Home | Ignite Shop</title>
       </Head>
 
-      <h1>Hello World</h1>
+      <HomeContainer ref={sliderRef} className="keen-slider">
+        {products.map(({ id, name, price, imageUrl }) => (
+          <Link key={id} prefetch={false} href={`/product/${id}`}>
+            <Product className="keen-slider__slide">
+              <Image src={imageUrl} width={560} height={480} alt="" />
+              <footer>
+                <div>
+                  <strong>{name}</strong>
+                  <span>{price}</span>
+                </div>
+                <Icon onClick={handleAddToCart}>
+                  <Bag size={24} />
+                </Icon>
+              </footer>
+            </Product>
+          </Link>
+        ))}
+      </HomeContainer>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await stripe.products.list({
+    expand: ['data.default_price'],
+  })
+
+  const products = data.map((product) => {
+    const price = product.default_price as Stripe.Price
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: priceFormatter.format(price.unit_amount / 100),
+    }
+  })
+
+  const productsData = products.filter(
+    (product) => product.name !== 'Subscription',
+  )
+
+  return {
+    props: {
+      products: productsData,
+    },
+    revalidate: 60 * 60 * 5, // 5 hours
+  }
 }
---
+++ src/styles/pages/home.ts
@@ -0,0 +1,97 @@
+import { styled } from "..";
+
+export const HomeContainer = styled('main', {
+  display: 'flex',
+  width: '100%',
+  maxWidth: 'calc(100vw - ((100vw - 1180px)/2))',
+  marginLeft: 'auto',
+  minHeight: 656,
+  overflow: 'hidden',
+})
+
+export const Product = styled('div', {
+  background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
+  borderRadius: 8,
+  cursor: 'pointer',
+  position: 'relative',
+  overflow: 'hidden',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  img: {
+    objectFit: 'cover',
+  },
+
+  footer: {
+    position: 'absolute',
+    bottom: '0.25rem',
+    left: '0.25rem',
+    right: '0.25rem',
+    padding: '1.25rem',
+    paddingRight: '2rem',
+    borderRadius: 6,
+
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+
+    backgroundColor: 'rgba(0,0,0,0.6)',
+    transform: 'translateY(110%)',
+    opacity: 0,
+    transition: 'all 200ms ease-in-out',
+
+    div: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'flex-start',
+      justifyContent: 'center',
+      gap: '0.25rem',
+    },
+  },
+
+  strong: {
+    fontSize: '$lg',
+    color: '$gray100',
+  },
+
+  span: {
+    fontSize: '$xl',
+    fontWeight: 'bold',
+    color: '$green300',
+  },
+
+  '&:hover': {
+    footer: {
+      transform: 'translateY(0)',
+      opacity: 1,
+    },
+  },
+})
+
+export const Icon = styled('button', {
+  border: 0,
+  position: 'relative',
+  borderRadius: 6,
+  padding: '0.75rem',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  backgroundColor: '$green500',
+  cursor: 'pointer',
+
+  svg: {
+    color: '$gray300',
+  },
+
+  transition: 'all 200ms ease-in-out',
+
+  '&:hover': {
+    backgroundColor: '$green300',
+    svg: {
+      color: '$gray100',
+    },
+  },
+})
