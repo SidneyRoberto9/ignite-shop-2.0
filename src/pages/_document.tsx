9df05ff98d0b4231456527f81b5d39446a52222b
SidneyRoberto9 <sidneyrpsilva@gmail.com>
2023-01-25 13:42:42 -0300

    ✨ feat: Create and use styles instace with stitches

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,28 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from "next/document";
+
+import { getCssText } from "../styles";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="icon" type="image/x-icon" href="/logo.ico" />
+
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
