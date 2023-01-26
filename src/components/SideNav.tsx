3487be9f41702c3936c9fbc19d6dd6cb8c91f951
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-01-26 12:14:55 -0300

    ✨ feat: Create SideNav Component

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,54 +1,71 @@
-import Image from 'next/image'
-import { useState } from 'react'
-import { slide as Menu, State } from 'react-burger-menu'
+import Image from "next/image";
+import { useContext } from "react";
+import { slide as Menu } from "react-burger-menu";
 
-import svg from '../assets/test.svg'
-import {
-  Button,
-  Content,
-  Item,
-  ItemsContainer,
-  SideNavContainer,
-} from '../styles/components/sidenav'
+import { ShoppingCart } from "../context/ShoppingCartContext";
+import { SideNavController } from "../context/SideNavControllerContext";
+import { Button, Content, Item, ItemsContainer, SideNavContainer } from "../styles/components/sidenav";
+import { priceFormatter } from "../util/format";
 
-interface SideNavProps {
-  isOpen: boolean
-  setIsOpen: (isOpen: boolean) => void
-}
+export function SideNav() {
+  const { toggleSideNav, isOpen } = useContext(SideNavController)
+  const { productList, removeToCart, valueTotal, productSize } =
+    useContext(ShoppingCart)
 
-export function SideNav({ isOpen, setIsOpen }: SideNavProps) {
-  function handleStateChange(state: State) {
-    setIsOpen(state.isOpen)
-  }
+  const shoppingCartIsEmpty = productSize == 0
+
+  // async function handleBuyProduct() {
+  //   try {
+  //     setIsCreatingCheckoutSession(true)
+  //     const { data } = await axios.post('/api/checkout', {
+  //       priceId: defaultPriceId,
+  //     })
+
+  //     window.location.href = data.checkoutUrl
+  //   } catch (error) {
+  //     //Datadog / Sentry
+  //     setIsCreatingCheckoutSession(false)
+  //     alert('Falha ao redirecionar ao checkout!')
+  //   }
+  // }
 
   return (
     <SideNavContainer>
       <Menu
         right
         isOpen={isOpen}
-        width={'30rem'}
+        width={'32rem'}
         customBurgerIcon={false}
-        onStateChange={(state) => handleStateChange(state)}
+        onStateChange={(state) => toggleSideNav(state.isOpen)}
         className="side-nav"
       >
         <div>
           <h1>Sacola de Compras</h1>
 
           <ItemsContainer>
-            <Item>
-              <ins>
-                <Image src={svg} alt="" width={100} height={93} />
-              </ins>
+            {productList.map((product) => (
+              <Item id={product.id}>
+                <ins>
+                  <Image
+                    src={product.imageUrl}
+                    alt=""
+                    width={100}
+                    height={93}
+                  />
+                </ins>
 
-              <article>
-                <small>
-                  <p>Camiseta Beyond the Limits</p>
-                  <strong>R$ 79,90</strong>
-                </small>
+                <article>
+                  <small>
+                    <p>{product.name}</p>
+                    <strong>
+                      {priceFormatter.format(product.price / 100)}
+                    </strong>
+                  </small>
 
-                <span>remover</span>
-              </article>
-            </Item>
+                  <span onClick={() => removeToCart(product.id)}>remover</span>
+                </article>
+              </Item>
+            ))}
           </ItemsContainer>
         </div>
 
@@ -56,14 +73,14 @@ export function SideNav({ isOpen, setIsOpen }: SideNavProps) {
           <article>
             <p>
               <span>Quantidade</span>
-              <small>3 itens</small>
+              <small>{productSize} itens</small>
             </p>
             <p>
               <strong>Valor Total</strong>
-              <ins>R$ 270,00</ins>
+              <ins>{priceFormatter.format(valueTotal / 100)}</ins>
             </p>
           </article>
-          <Button>Finalizar Compra</Button>
+          <Button disabled={shoppingCartIsEmpty}>Finalizar Compra</Button>
         </Content>
       </Menu>
     </SideNavContainer>
