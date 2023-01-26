635bbf4682e27a0d3e7a6a4e967bcfa7eb0de72d
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-01-26 12:15:11 -0300

    ✨ feat: Add Providers for app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,23 @@
-import { useState } from 'react'
-
-import { Header } from '../components/Header'
-import { SideNav } from '../components/SideNav'
-import { globalStyles } from '../styles/global'
-import { Container } from '../styles/pages/app'
+import { Header } from "../components/Header";
+import { SideNav } from "../components/SideNav";
+import { ShoppingCartProvider } from "../context/ShoppingCartContext";
+import { SideNavControllerProvider } from "../context/SideNavControllerContext";
+import { globalStyles } from "../styles/global";
+import { Container } from "../styles/pages/app";
 
 import type { AppProps } from 'next/app'
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [isOpen, setIsOpen] = useState(false)
-
-  function handleOpenSideNav() {
-    setIsOpen(true)
-  }
-
   return (
-    <Container>
-      <SideNav isOpen={isOpen} setIsOpen={setIsOpen} />
-      <Header handleOpenSideNav={handleOpenSideNav} />
-      <Component {...pageProps} id="page-wrap" />
-    </Container>
+    <ShoppingCartProvider>
+      <SideNavControllerProvider>
+        <Container>
+          <SideNav />
+          <Header />
+          <Component {...pageProps} />
+        </Container>
+      </SideNavControllerProvider>
+    </ShoppingCartProvider>
   )
 }
